d25cbd4566373aa2a44c8968bd7a82b48fc8f678
jroa <jpablonano@gmail.com>
2024-09-01 19:53:43 -0500

    feat(): Add set config by env file

--- py_db/config/sql_connection_config.py
+++ py_db/config/sql_connection_config.py
@@ -1,5 +1,6 @@
 # config/sql_connection _config.py
 
+from dotenv import load_dotenv, dotenv_values
 from typing import Optional
 from .base_connection_config import BaseConnectionConfig
 
@@ -31,3 +32,59 @@ class SqlConnectionConfig(BaseConnectionConfig):
         self.password = password
         self.database = database
         self.port = port
+
+    env_file_path: str = ".env"
+
+    @classmethod
+    def set_env_config_file(cls, file_path: str) -> None:
+        """
+        Set the path of configuration file
+
+        :param file_path: Path to configuration file, by default is '.env'.
+        """
+        cls.env_file_path = file_path
+
+    @classmethod
+    def load_credential(cls, credential_name: str, throw_on_error: bool = True):
+        """
+        Load the specified credential into a new class instance
+
+        :param credential_name: Name of the credential, it must match with the prefix (followed by '_') of the defined variables.
+        :param throw_on_error: Determines if on an error, the function throws up an exception
+        """
+        load_dotenv(dotenv_path=cls.env_file_path)
+
+        config_values = dotenv_values()
+
+        class_variables = [
+            "host",
+            "user",
+            "password",
+            "database",
+            "port",
+        ]
+
+        config_variable_names = [
+            f"{credential_name.upper()}_{variable}" for variable in class_variables
+        ]
+
+        if throw_on_error & (
+            len(
+                [
+                    variable_name
+                    for variable_name in config_values.keys()
+                    if variable_name.upper() in config_variable_names
+                ]
+            )
+            == 0
+        ):
+            raise Exception(
+                "There no connection keys that satisfy the credential syntax. '{credential name}_{variable name}'"
+            )
+
+        return cls(
+            **{
+                variable: config_values.get(f"{credential_name.upper()}_{variable}")
+                for variable in class_variables
+            }
+        )
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py_db",
-    version="1.2",
+    version="1.3",
     packages=find_packages(),
     install_requires=[
         "mysql-connector-python==9.0.0",
